57d97bca1fa52e4f37a5dbca73ed4ec07b5a94ae
Guilherme Correa Melos <melos@Guilhermes-MacBook-Pro.local>
2023-06-05 20:50:31 -0300

    add

--- api/server/core/domain/repository/RelationshipRepository.py
+++ api/server/core/domain/repository/RelationshipRepository.py
@@ -3,10 +3,10 @@ from sqlalchemy.orm import Session
 from ..entity import models
 
 
-class RecipeRepository:
+class RelationshipRepository:
     def __init__(self, session: Session) -> None:
         self.session = session
 
     def get_by_uuid(self, uuid: str) -> models.Relationship:
         stmt = select(models.Relationship).where(models.Relationship.uuid == uuid)
-        return self.session.execute(stmt).first()
+        return self.session.execute(stmt)
--- api/server/core/service/UserService.py
+++ api/server/core/service/UserService.py
@@ -1,3 +1,7 @@
+from domain.repository.UserRepository import UserRepository
+from domain.repository.RelationshipRepository import RelationshipRepository
+
 class UserRepository:
-    def __init__(self):
-        pass
+    def __init__(self, user_repo: UserRepository, relation_repo: RelationshipRepository):
+        self.user_repo = user_repo
+        self.relation_repo = relation_repo
